b51aa6eec6c1bc315d6800cf2413bf504ac2aa2d
Gathoni-K <gathonikarume@gmail.com>
2025-12-10 22:06:52 +0300

    Login and sign up pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+import NavBar from "./components/layout/NavBar";
+
 
 function App() {
   
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+    // <BrowserRouter>
+    // <Routes>
+    //   <Route path="/" element={<Login />} />
+    //   <Route path="/signup" element={<SignUp />} />
 
       
-    </Routes>
-    </BrowserRouter>
+    // </Routes>
+    // </BrowserRouter>
+
+    <NavBar />
+
   )
 }
 
---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,21 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            
+            {/* logo and brand name */}
+            <div>
+                <img src="" />
+                <h1>OtakuBoxd</h1>
+            </div>
+
+            {/* links section */}
+            <div>
+                <a></a>
+            </div>
+        </div>
+    )
+    }
+
+export default NavBar
